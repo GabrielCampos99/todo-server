bde9b87c6f82e416ac3338789278e73379fcbd82
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-10-24 21:28:06 -0300

    feat: User entity

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn({ type: "bigint" })
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
