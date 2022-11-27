ad52383f901953caeddecc0d305bb4f4695f09b0
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-11-26 21:43:28 -0300

    for prod

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 export default {
   type: process.env.DB_NAME ? process.env.DB_NAME : "postgres",
   host: process.env.DB_HOST ? process.env.DB_HOST : "localhost",
@@ -5,9 +6,9 @@ export default {
   username: process.env.DB_USER ? process.env.DB_USER : "postgres",
   password: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : "postgres",
   database: "todo",
-  entities: ["./src/modules/**/typeorm/entities/*.ts"],
-  migrations: ["./src/shared/typeorm/migrations/*.ts"],
+  entities: ["./dist/modules/**/typeorm/entities/*.js"],
+  migrations: ["./dist/shared/typeorm/migrations/*.js"],
   cli: {
-    migrationsDir: "./src/shared/typeorm/migrations",
+    migrationsDir: "./dist/shared/typeorm/migrations",
   },
 };
