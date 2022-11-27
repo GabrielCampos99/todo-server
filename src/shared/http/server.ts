99fb3337f0b5c991edfe210c228d2aacd14fba2d
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-11-26 22:36:59 -0300

    fix port

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -10,6 +10,7 @@ import routes from "./routes/index";
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
@@ -30,7 +31,8 @@ app.use(
     });
   }
 );
+const port = process.env.PORT || 8080;
 
-app.listen(3333, () => {
-  console.log("Server started on port 3333! 🏆");
+app.listen(port, () => {
+  console.log(`Server started on port ${port} ou magica! 🏆`);
 });
